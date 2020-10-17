e2cd66349879a486a605a9588bc53a01ed10f0e0
Tsiamitros <kos.tsiamitros@gmail.com>
2020-10-17 14:41:58 +0300

    Add project files.

---
+++ EightQueens/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace EightQueens
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.bruteForceButton = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.methodLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.exeTimeLabel = new System.Windows.Forms.Label();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.barChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.exeTestButton = new System.Windows.Forms.Button();
+            this.inputSeedTextBox = new System.Windows.Forms.TextBox();
+            this.inputSeedLabel = new System.Windows.Forms.Label();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.barChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bruteForceButton
+            // 
+            this.bruteForceButton.Location = new System.Drawing.Point(116, 35);
+            this.bruteForceButton.Name = "bruteForceButton";
+            this.bruteForceButton.Size = new System.Drawing.Size(75, 23);
+            this.bruteForceButton.TabIndex = 0;
+            this.bruteForceButton.Text = "Brute Force";
+            this.bruteForceButton.UseVisualStyleBackColor = true;
+            this.bruteForceButton.Click += new System.EventHandler(this.bruteForceButton_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(53, 32);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(450, 450);
+            this.flowLayoutPanel1.TabIndex = 65;
+            // 
+            // methodLabel
+            // 
+            this.methodLabel.AutoSize = true;
+            this.methodLabel.Location = new System.Drawing.Point(3, 0);
+            this.methodLabel.Name = "methodLabel";
+            this.methodLabel.Size = new System.Drawing.Size(74, 13);
+            this.methodLabel.TabIndex = 66;
+            this.methodLabel.Text = "Method Used:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 67;
+            this.label1.Text = "Execution Time:";
+            // 
+            // exeTimeLabel
+            // 
+            this.exeTimeLabel.AutoSize = true;
+            this.exeTimeLabel.Location = new System.Drawing.Point(3, 26);
+            this.exeTimeLabel.Name = "exeTimeLabel";
+            this.exeTimeLabel.Size = new System.Drawing.Size(35, 13);
+            this.exeTimeLabel.TabIndex = 68;
+            this.exeTimeLabel.Text = "label2";
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.methodLabel);
+            this.flowLayoutPanel2.Controls.Add(this.label1);
+            this.flowLayoutPanel2.Controls.Add(this.exeTimeLabel);
+            this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(2, 66);
+            this.flowLayoutPanel2.Margin = new System.Windows.Forms.Padding(15);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(200, 100);
+            this.flowLayoutPanel2.TabIndex = 69;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(220, 25);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(568, 533);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.flowLayoutPanel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(560, 507);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.barChart);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(560, 507);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // barChart
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.barChart.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.barChart.Legends.Add(legend3);
+            this.barChart.Location = new System.Drawing.Point(18, 45);
+            this.barChart.Name = "barChart";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.barChart.Series.Add(series3);
+            this.barChart.Size = new System.Drawing.Size(525, 300);
+            this.barChart.TabIndex = 0;
+            this.barChart.Text = "chart1";
+            // 
+            // exeTestButton
+            // 
+            this.exeTestButton.Location = new System.Drawing.Point(12, 35);
+            this.exeTestButton.Name = "exeTestButton";
+            this.exeTestButton.Size = new System.Drawing.Size(88, 23);
+            this.exeTestButton.TabIndex = 70;
+            this.exeTestButton.Text = "Execute Test";
+            this.exeTestButton.UseVisualStyleBackColor = true;
+            this.exeTestButton.Click += new System.EventHandler(this.exeTestButton_Click);
+            // 
+            // inputSeedTextBox
+            // 
+            this.inputSeedTextBox.Location = new System.Drawing.Point(8, 184);
+            this.inputSeedTextBox.Name = "inputSeedTextBox";
+            this.inputSeedTextBox.Size = new System.Drawing.Size(100, 20);
+            this.inputSeedTextBox.TabIndex = 69;
+            // 
+            // inputSeedLabel
+            // 
+            this.inputSeedLabel.AutoSize = true;
+            this.inputSeedLabel.Location = new System.Drawing.Point(5, 168);
+            this.inputSeedLabel.Name = "inputSeedLabel";
+            this.inputSeedLabel.Size = new System.Drawing.Size(172, 13);
+            this.inputSeedLabel.TabIndex = 70;
+            this.inputSeedLabel.Text = "Input seed (integer - for indiv. tests)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 584);
+            this.Controls.Add(this.exeTestButton);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.inputSeedLabel);
+            this.Controls.Add(this.bruteForceButton);
+            this.Controls.Add(this.inputSeedTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.barChart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bruteForceButton;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label methodLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label exeTimeLabel;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart barChart;
+        private System.Windows.Forms.Button exeTestButton;
+        private System.Windows.Forms.Label inputSeedLabel;
+        private System.Windows.Forms.TextBox inputSeedTextBox;
+    }
+}
+
---
+++ EightQueens/Form1.cs
@@ -0,0 +1,481 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace EightQueens
+{
+    public partial class Form1 : Form
+    {
+        #region Declarations
+        //main variables
+        private int[,] board;
+        private int queenNum;
+        //2 rows and queenNum columns
+        //For each queen:
+        //row 0 has posX
+        //row 1 has posY
+        private int[,] queenPos;
+        //INdex of last queen (used column) in queenPos
+        private int queenIndex;
+
+        //for reproducible results
+        private int seed;
+
+        //metrics
+        private long checkSolutionMs;
+
+        //Visuals
+        private PictureBox[,] pictureBoxes;
+
+        //Labels
+        private string exeTimeText;
+
+        #endregion
+
+        #region Initialization
+        public Form1()
+        {
+            InitializeComponent();
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            InitializeParameters();
+            checkSolutionMs = 0;
+            pictureBoxes = new PictureBox[8, 8];
+            //set the Random seed
+            this.seed = 1;
+
+            watch.Stop();
+            var elapsed = watch.ElapsedMilliseconds;
+            Console.WriteLine("Initialization time: " + elapsed);
+
+        }
+
+        private void InitializeParameters()
+        {
+            //Init the 8x8 array with zeros
+            this.board = new int[8, 8];
+
+            //set queen number
+            this.queenNum = 8;
+
+            //initialize queen position dictionary
+            this.queenPos = new int[2, queenNum];
+
+            //reset index
+            this.queenIndex = 0;
+
+            //clear strings
+            this.exeTimeText = "";
+        }
+
+        #endregion
+
+        #region Brute Force solution
+        //TODO: the function as is doesn't avoid checking same solutions
+        private void FindSolutionBruteForce(int inputSeed)
+        {
+            Console.WriteLine("seed used for this iteration: " + inputSeed);
+            Random rnd = new Random(inputSeed);
+            bool flag = false;
+            bool terminationFlag;
+
+            //Untill you have a solution
+            do
+            {
+                //Reinitialize for a new solution
+                InitializeParameters();
+                //For all queens generate random position
+                for (int i = 0; i < this.queenNum; i++)
+                {
+                    flag = false;
+                    do
+                    {
+                        //Generate random position in [0,8)
+                        int posX = rnd.Next(0, 8);
+                        int posY = rnd.Next(0, 8);
+                        //CHeck if the generated position is occupied
+                        if (this.board[posX, posY] == 1)
+                        {
+                            //discard this position
+                            flag = false;
+                        }
+                        else
+                        {
+                            //Update the board
+                            this.board[posX, posY] = 1;
+                            //Add a new queen position to the list
+                            this.queenPos[0, queenIndex] = posX;
+                            this.queenPos[1, queenIndex] = posY;
+                            this.queenIndex++;
+                            flag = true;
+                        }
+                    } while (flag == false);
+                }
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                terminationFlag = CheckSolution();
+                watch.Stop();
+                checkSolutionMs += watch.ElapsedMilliseconds;
+                
+            } while (terminationFlag == true);
+            Console.WriteLine("Found Solution!");
+
+            PrintSolution();
+            Console.WriteLine("CheckSolution total time: " + checkSolutionMs);
+            exeTimeText += "CheckSolution total time: " + checkSolutionMs + " ms" + "\n";
+        }
+
+        #endregion
+
+        #region Print solution to console
+        private void PrintSolution()
+        {
+            string str = "";
+            for (int i = 0; i < this.board.GetLength(0); i++)
+            {
+                str = "";
+                for (int j = 0; j < this.board.GetLength(1); j++)
+                {
+                    if(this.board[i,j] == 1)
+                    {
+                        str += "X";
+                    }
+                    else
+                    {
+                        str += "0";
+                    }
+                }
+                Console.WriteLine(str);
+            }
+        }
+
+        #endregion
+
+        #region  Check if current solution is acceptable
+        private bool CheckSolution()
+        {
+            bool returnFlag = false;
+
+            //Check all queen positions for conflicts
+            for (int j = 0; j < this.queenPos.GetLength(1); j++)
+            {
+                if (CheckHorizontal(queenPos[0, j], queenPos[1, j]) == true)
+                {
+                    returnFlag = true;
+                    break;
+                }
+                if (CheckVertical(queenPos[0, j], queenPos[1, j]) == true)
+                {
+                    returnFlag = true;
+                    break;
+                }
+                if (CheckDiagonal(queenPos[0, j], queenPos[1, j]) == true)
+                {
+                    returnFlag = true;
+                    break;
+                }
+            }           
+
+            return returnFlag;
+        }
+
+        //Returns true if there is a horizontal collision
+        private bool CheckHorizontal(int x, int y)
+        {
+            bool returnFlag = false;
+
+            //For given x position check all columns except the current
+            for (int i = 0; i < this.board.GetLength(1); i++)
+            {
+                //Exclude the current column
+                if( i != y )
+                {
+                    if (this.board[x, i] == 1)
+                    {
+                        returnFlag = true;
+                        break;
+                    }
+                }
+
+            }
+
+            return returnFlag;
+        }
+
+        //Returns true if there is a vertical collision
+        private bool CheckVertical(int x, int y)
+        {
+            bool returnFlag = false;
+
+            //For given x position check all rows except the current
+            for (int i = 0; i < this.board.GetLength(0); i++)
+            {
+                //Exclude the current column
+                if (i != x)
+                {
+                    if (this.board[i, y] == 1)
+                    {
+                        returnFlag = true;
+                        break;
+                    }
+                }
+
+            }
+
+            return returnFlag;
+        }
+
+        //Returns true if there is a diagonal collision
+        private bool CheckDiagonal(int x, int y)
+        {
+            //if there is at least one collision return true
+            //NOTE: I could have checked it in a single statement 
+            //but I think this way the execution time is less in terms of O()
+            //because there is the possibility that not all functions will get called
+            //if a conflict is found earlier in every iteration
+            if(CheckUpperLeftDiagonal(x, y) == true)
+            {
+                return true;
+            }
+            if (CheckLowerRightDiagonal(x, y) == true)
+            {
+                return true;
+            }
+            if(CheckLowerLeftDiagonal(x, y) == true)
+            {
+                return true;
+            }
+            if(CheckUpperRightDiagonal(x, y) == true)
+            {
+                return true;
+            }
+
+            //If you got to this point then there are no conflicts so return false
+            return false;
+
+        }
+
+        private bool CheckUpperLeftDiagonal(int x, int y)
+        {
+            bool returnFlag = false;
+
+            for (int i = 1; i < this.board.GetLength(0); i++)
+            {
+                if( ((x-i) < 0) || ((y -i) < 0) )
+                {
+                    //Out of bounds
+                    break;
+                }
+                if(this.board[x - i, y - i] == 1)
+                {
+                    returnFlag = true;
+                    break;
+                }
+            }
+
+            return returnFlag;
+        }
+
+        private bool CheckLowerRightDiagonal(int x, int y)
+        {
+            bool returnFlag = false;
+
+            for (int i = 1; i < this.board.GetLength(0); i++)
+            {
+                if (((x + i) >= this.board.GetLength(0)) || ((y + i) >= this.board.GetLength(1)))
+                {
+                    //Out of bounds
+                    break;
+                }
+                if (this.board[x + i, y + i] == 1)
+                {
+                    returnFlag = true;
+                    break;
+                }
+            }
+
+            return returnFlag;
+        }
+
+        private bool CheckLowerLeftDiagonal(int x, int y)
+        {
+            bool returnFlag = false;
+
+            for (int i = 1; i < this.board.GetLength(0); i++)
+            {
+                if (((x + i) >= this.board.GetLength(0)) || ((y - i) < 0))
+                {
+                    //Out of bounds
+                    break;
+                }
+                if (this.board[x + i, y - i] == 1)
+                {
+                    returnFlag = true;
+                    break;
+                }
+            }
+
+            return returnFlag;
+        }
+
+        private bool CheckUpperRightDiagonal(int x, int y)
+        {
+            bool returnFlag = false;
+
+            for (int i = 1; i < this.board.GetLength(0); i++)
+            {
+                if ( ((x - i) < 0) || ((y + i) >= this.board.GetLength(1)) )
+                {
+                    //Out of bounds
+                    break;
+                }
+                if (this.board[x - i, y + i] == 1)
+                {
+                    returnFlag = true;
+                    break;
+                }
+            }
+
+            return returnFlag;
+        }
+        #endregion
+
+        #region Button click handlers
+        private void bruteForceButton_Click(object sender, EventArgs e)
+        {
+            int inputSeed = ReadSeed();
+            Console.WriteLine("Input seed " + inputSeed);
+            BruteForceWrapper(inputSeed);
+        }
+
+        private void exeTestButton_Click(object sender, EventArgs e)
+        {
+            TestBench();
+        }
+
+        //Returns user seed or 1 if not defined
+        private int ReadSeed()
+        {
+            int userSeed = 1;
+            if (!string.IsNullOrEmpty(inputSeedTextBox.Text))
+            {
+                try
+                {
+                    userSeed = int.Parse(inputSeedTextBox.Text);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return userSeed;
+        }
+        #endregion
+
+        #region Wrappers
+        private void BruteForceWrapper(int inputSeed)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            FindSolutionBruteForce(inputSeed);
+            watch.Stop();
+            var elapsed = watch.ElapsedMilliseconds;
+
+            Console.WriteLine("FindSolutionBruteForce total time: " + elapsed + " ms");
+            Console.WriteLine("New solution generation total time: " + (elapsed - checkSolutionMs) + " ms");
+            exeTimeText += "FindSolutionBruteForce total time: " + elapsed + " ms" + "\n";
+            exeTimeText += "New solution generation total time: " + (elapsed - checkSolutionMs) + " ms";
+            this.barChartY.Add(elapsed);
+
+            BruteForceUpdateTexts();
+
+            InitializePictureBoxes();
+        }
+        #endregion
+
+        #region Miscelaneous
+        private void BruteForceUpdateTexts()
+        {
+            methodLabel.Text = "Method Used: Brute Force";
+            exeTimeLabel.Text = exeTimeText;
+        }
+
+        List<int> barChartX = new List<int>();
+        List<long> barChartY = new List<long>();
+
+        private void UpdateBarChart()
+        {
+            barChart.Series[0].LegendText = "Brute Force Statistics";
+            barChart.Series[0].ChartType = SeriesChartType.Column;
+            barChart.Series[0].IsValueShownAsLabel = true;
+            barChart.Series[0].Points.DataBindXY(barChartX, barChartY);
+        }
+
+        private void ReinitializePanel()
+        {
+            if (flowLayoutPanel1 != null)
+            {
+                List<Control> listControls = flowLayoutPanel1.Controls.Cast<Control>().ToList();
+
+                foreach (Control control in listControls)
+                {
+                    flowLayoutPanel1.Controls.Remove(control);
+                    control.Dispose();
+                }
+
+                //One liner for the same thing
+                //flowLayoutPanel1.Controls.OfType<PictureBox>().ToList().ForEach(btn => btn.Dispose());
+            }
+        }
+
+        private void InitializePictureBoxes()
+        {
+            ReinitializePanel();
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    pictureBoxes[i, j] = new PictureBox();
+                    if (this.board[i, j] == 1)
+                    {
+                        pictureBoxes[i, j].Image = EightQueens.Properties.Resources.queen50x50NoBackground;
+                    }
+                    //Pick a different color for the different tiles
+                    if ((i + j == 1) || (i + j == 3) || (i + j == 5) || (i + j == 7) || (i + j == 9) || (i + j == 11) || (i + j == 13))
+                    {
+                        pictureBoxes[i, j].BackColor = Color.Red;
+                    }
+                    else
+                    {
+                        pictureBoxes[i, j].BackColor = Color.Green;
+                    }
+                    pictureBoxes[i, j].Size = new Size(50, 50);
+                    flowLayoutPanel1.Controls.Add(pictureBoxes[i, j]);
+                }
+            }
+        }
+        #endregion
+
+        #region Testbench
+        private void TestBench()
+        {
+            int maxIter = 15;
+            for (int i = 0; i < maxIter; i++)
+            {
+                Console.WriteLine(i + "/" + maxIter + " iteration");
+
+                BruteForceWrapper(seed);
+
+                this.barChartX.Add(seed);
+                this.seed++;
+            }
+            //Console.WriteLine("X length: " + barChartX.Count);
+            //Console.WriteLine("Y length: " + barChartY.Count);
+            UpdateBarChart();
+        }
+        #endregion
+    }
+}
